585698a1aa63b6d3d2a8ab8ff94b4ab0e7e815d5
Sai Lakshmi <sailakshmi22.p@gmail.com>
2022-09-22 14:55:11 -0400

    Initial Commit

---
+++ SplashScreen/SplashScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplashScreenApp.swift
+//  SplashScreen
+//
+//  Created by Sai Lakshmi on 9/22/22.
+//
+
+import SwiftUI
+
+@main
+struct SplashScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
